6f17fa250a2c7619e3af4ae067defb0e1a1e42ef
renatogm24 <renato.garay@pucp.pe>
2022-01-18 09:54:22 -0500

    first commit

---
+++ modularization/parent.py
@@ -0,0 +1,18 @@
+local_val = "Magic unicorns"
+def square(x):
+    return x * x
+class User:
+    def __init__(self, name):
+        self.name = name
+    def say_hello(self):
+        return "hello"
+
+print(square(5))
+user = User("Anna")
+print(user.name)
+print(user.say_hello())
+print(__name__)
+if __name__ == "__main__":
+    print("el archivo se está ejecutando directamente")
+else:
+    print("El archivo se está ejecutando porque es importado por otro archivo. El archivo se llama:", __name__)
